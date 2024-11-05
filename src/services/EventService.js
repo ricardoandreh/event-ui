9a2c6a75f3fc01b87b4e6af105293b4c69c6949a
Ricardo André <ricardo.andre.ifc@gmail.com>
2024-11-05 15:05:03 -0300

    refactor: redux action & axios interceptors

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,21 +1,49 @@
 import axios from "axios";
+import { fetchRefreshToken } from "../redux/auth/authSlice";
+import { store } from "../redux/store";
 
 const apiClient = axios.create({
   baseURL: `${import.meta.env.VITE_API_URL}/api`,
-  withCredentials: false,
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
   },
 });
 
-// apiClient.interceptors.request.use(async (config) => {});
+let refreshTokenPromise = null;
+
+apiClient.interceptors.request.use(
+  async (config) => {
+    const { auth } = store.getState();
+
+    if (auth?.token) {
+      const isTokenValid = Date.now() < auth.expiration;
+
+      if (!isTokenValid) {
+        if (!refreshTokenPromise) {
+          refreshTokenPromise = store.dispatch(fetchRefreshToken());
+        }
+
+        await refreshTokenPromise;
+        refreshTokenPromise = null;
+      }
+
+      const { token } = store.getState().auth;
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default {
   getEvents() {
     return apiClient.get("/events");
   },
   getEvent(id) {
-    return apiClient.get("/events/" + id);
+    return apiClient.get(`/events/${id}`);
   },
 };
